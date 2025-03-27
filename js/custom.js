7a4a33a9680168c285e77b7e92411a837ef3b8a8
Strahinja Ninkov <ninkovs18@gmail.com>
2025-03-28 00:39:38 +0100

    izmene na view i dodato email service

--- js/custom.js
+++ js/custom.js
@@ -26,3 +26,46 @@
     dots: true,
   });
 })(window.jQuery);
+
+function clearInputs() {
+  document.getElementById("name").value = "";
+  document.getElementById("message").value = "";
+  document.getElementById("subject").value = "";
+  document.getElementById("email").value = "";
+}
+
+function sendEmail() {
+  emailjs.send("service_nw9l3ji", "template_6onqinf", {
+    name: document.getElementById("name").value,
+    message: document.getElementById("message").value,
+    subject: document.getElementById("subject").value,
+    email: document.getElementById("email").value,
+  });
+}
+
+const posalji = document.getElementById("posalji");
+
+function showMessage() {
+  let messageElement = document.getElementById("status-message");
+  messageElement.style.display = "block";
+  messageElement.style.opacity = "1";
+
+  setTimeout(function () {
+    let fadeEffect = setInterval(function () {
+      if (messageElement.style.opacity > "0") {
+        messageElement.style.opacity -= 0.1;
+      } else {
+        clearInterval(fadeEffect);
+        messageElement.style.display = "none";
+      }
+    }, 100);
+  }, 4000);
+}
+
+posalji.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  sendEmail();
+  clearInputs();
+  showMessage();
+});
